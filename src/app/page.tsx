aecd3619f420f8eb8f95b8e95fe1dfaa151c5a9e
henriquenascimento01 <henriquenascimento300@hotmail.com>
2024-05-14 15:46:30 -0300

    [FEA]Ajuste no readme e ajuste para conter botão na tela inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+'use client'
+
 import styles from "./page.module.css";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main className={styles.main}>
-      <h1>Olá mundo</h1>
+      <button  onClick={() => router.push('/posts')} style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: '#007BFF', color: 'white', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>
+        Iniciar
+      </button>
     </main>
   );
 }
